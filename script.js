9c6b34f8a67c0b60d53d032146088681f2ceb0cd
Seyedhooman Hesamyan <seyedhooman_hesamyan@edu.aua.am>
2017-09-06 20:23:45 -0700

    my first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+setTimeout(function() {
+    var x = 10;
+    var y = 20;
+
+    // alert(x + y);
+    console.log(x + y);
+
+    var name = window.prompt("Enter your name?");
+    alert(name);
+
+    document.getElementById("name").innerHTML = name;
+}, 100);
